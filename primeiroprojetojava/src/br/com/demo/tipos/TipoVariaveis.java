6be6303791d266f0d6292105ecdb90785b6231f9
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2021-10-09 13:59:51 -0300

    tipos de variavéis primitivas

---
+++ primeiroprojetojava/src/br/com/demo/tipos/TipoVariaveis.java
@@ -0,0 +1,25 @@
+package br.com.demo.tipos;
+
+public class TipoVariaveis {
+    public static void main(String[] args) {
+        /*
+         * Estes são os tipos primitivos de dados do Java
+         */
+        byte tipoByte = 123;
+        short tipoShort = 1234;
+        char tipoChar = 'a';
+        float tipoFloat = 1.2F;
+        double tipoDouble = 1.2;
+        int tipoInt = 1000000000;
+        long tipoLong = 2000000000000000000L;
+        boolean tipoBoolean = true | false;
+        System.out.println("Valor do tipoByte = " + tipoByte);
+        System.out.println("Valor do tipoShort = " + tipoShort);
+        System.out.println("Valor do tipoChar = "  + tipoChar);
+        System.out.println("Valor do tipoFloat = " + tipoFloat);
+        System.out.println("Valor do tipoDouble = " + tipoDouble);
+        System.out.println("Valor do tipoInt = " + tipoInt);
+        System.out.println("Valor do tipoLong = " + tipoLong);
+        System.out.println("Valor do tipoBoolean = " + tipoBoolean);
+    }
+}
